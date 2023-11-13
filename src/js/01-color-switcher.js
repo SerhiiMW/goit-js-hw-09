8c1a143c3fc0ee43f32c4a224ca750f3ffe51cfc
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-11-13 18:00:15 +0200

    hw1.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,7 +4,7 @@ const stopBtn = document.querySelector('button[data-stop]');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
-
+let timerId = null;
 
 startBtn.addEventListener("click", () => {
   timerId = setInterval(() => {
