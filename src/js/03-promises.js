d9287e84282d8aeffff5d807f9ae8ddd23876b8c
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2023-11-14 16:51:21 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,23 +2,26 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   form: document.querySelector('.form'),
-  button: document.querySelector('button[type="submit"]'),
   delay: document.querySelector('input[name="delay"]'),
   step: document.querySelector('input[name="step"]'),
   amount: document.querySelector('input[name="amount"]'),
 };
-
 refs.form.addEventListener('submit', startPromiseGener);
 
+let amount = 0;
+let step = 0;
+let delay = 0;
+let i = 0;
+let position = 0;
+
 function startPromiseGener(e) {
-  const amount = Number(refs.amount.value);
-  const step = Number(refs.step.value);
-  let delay = Number(refs.delay.value);
-  let i = 0;
-  let position = 0;
+  amount = Number(refs.amount.value);
+  step = Number(refs.step.value);
+  delay = Number(refs.delay.value);
   e.preventDefault();
   for (i = 0; i < amount; i += 1) {
     position += 1;
+
     createPromise(position, delay)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -26,18 +29,18 @@ function startPromiseGener(e) {
       .catch(({ position, delay }) => {
         Notify.success(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    function createPromise(position, delay) {
-      return new Promise((resolve, reject) => {
-        const shouldResolve = Math.random() > 0.3;
-        timerId = setTimeout(() => {
-          if (shouldResolve) {
-            resolve({ position, delay });
-          } else {
-            reject({ position, delay });
-          }
-        }, delay);
-      });
-    }
     delay += step;
   }
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    timerId = setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
